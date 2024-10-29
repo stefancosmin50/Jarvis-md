fe19742d59084d86dcb3bb9af388e441808bd32d
Ronak sharma <112383311+Ronak-xd@users.noreply.github.com>
2024-10-29 15:37:29 +0530

    fix

--- plugins/news.js
+++ plugins/news.js
@@ -1,44 +1,6 @@
 const { System, isPrivate, TechNews } = require("../lib/");
 
 System({
-  pattern: 'ndtvnews ?(.*)',
-  fromMe: isPrivate,
-  desc: 'get ndtv news',
-  type: 'news',
-}, async (message, match, m) => {
-  const apiUrl = 'https://api.guruapi.tech/news';
-  const res = await fetch(apiUrl);
-  const data = (await res.json()).result;
-  let formattedList = '';
-    data.slice(0, 5).forEach(item => {
-    const title = item.title || 'No Title Available';
-    const description = item.description || 'No Description Available';
-    const link = item.link || 'No Link Available';
-    formattedList += `*TITLE:* ${title}\n`;
-    formattedList += `*DESCRIPTION:* ${description}\n`;
-    formattedList += `*LINK:* ${link}\n\n`;
-  });
-  const buttonText = "Visit News";
-  const buttonUrl = data[0].link;
-  await message.send([
-    {
-      name: "cta_url",
-      display_text: buttonText,
-      url: buttonUrl,
-      merchant_url: buttonUrl,
-      action: "url",
-      icon: "",
-      style: "link"
-    }
-  ], {
-    body: formattedList,
-    footer: "*JARVIS-MD*",
-    title: "Top 5 News"
-  }, "button");
-});
-
-
-System({
   pattern: 'technews (.*)',
   fromMe: isPrivate,
   desc: 'Get tech news',
@@ -53,54 +15,51 @@ System({
     let newsItem;
     
     if (result.gadgets) {
-  const gadgets = result.gadgets;
-  const randomIndex = Math.floor(Math.random() * gadgets.length);
-  newsItem = gadgets[randomIndex];
-} else if (result.technology) {
-  const technology = result.technology;
-  const randomIndex = Math.floor(Math.random() * technology.length);
-  newsItem = technology[randomIndex];
-} else if (result.videos) {
-  const videos = result.videos;
-  const randomIndex = Math.floor(Math.random() * videos.length);
-  newsItem = videos[randomIndex];
-} else if (result.laptops) {
-  const laptops = result.laptops;
-  const randomIndex = Math.floor(Math.random() * laptops.length);
-  newsItem = laptops[randomIndex];
-} else if (result.reviews) {
-  const reviews = result.reviews;
-  const randomIndex = Math.floor(Math.random() * reviews.length);
-  newsItem = reviews[randomIndex];
-} else if (result.science) {
-  const science = result.science;
-  const randomIndex = Math.floor(Math.random() * science.length);
-  newsItem = science[randomIndex];
-} else if (result.gallery) {
-  const gallery = result.gallery;
-  const randomIndex = Math.floor(Math.random() * gallery.length);
-  newsItem = gallery[randomIndex];
-} else if (result.mobiles) {
-  const mobiles = result.mobiles;
-  const randomIndex = Math.floor(Math.random() * mobiles.length);
-  newsItem = mobiles[randomIndex];
-} else if (result.techook) {
-  const techook = result.techook;
-  const randomIndex = Math.floor(Math.random() * techook.length);
-  newsItem = techook[randomIndex];
-} else {
-  
-  newsItem = result;
-}
+      const gadgets = result.gadgets;
+      const randomIndex = Math.floor(Math.random() * gadgets.length);
+      newsItem = gadgets[randomIndex];
+    } else if (result.technology) {
+      const technology = result.technology;
+      const randomIndex = Math.floor(Math.random() * technology.length);
+      newsItem = technology[randomIndex];
+    } else if (result.videos) {
+      const videos = result.videos;
+      const randomIndex = Math.floor(Math.random() * videos.length);
+      newsItem = videos[randomIndex];
+    } else if (result.laptops) {
+      const laptops = result.laptops;
+      const randomIndex = Math.floor(Math.random() * laptops.length);
+      newsItem = laptops[randomIndex];
+    } else if (result.reviews) {
+      const reviews = result.reviews;
+      const randomIndex = Math.floor(Math.random() * reviews.length);
+      newsItem = reviews[randomIndex];
+    } else if (result.science) {
+      const science = result.science;
+      const randomIndex = Math.floor(Math.random() * science.length);
+      newsItem = science[randomIndex];
+    } else if (result.gallery) {
+      const gallery = result.gallery;
+      const randomIndex = Math.floor(Math.random() * gallery.length);
+      newsItem = gallery[randomIndex];
+    } else if (result.mobiles) {
+      const mobiles = result.mobiles;
+      const randomIndex = Math.floor(Math.random() * mobiles.length);
+      newsItem = mobiles[randomIndex];
+    } else if (result.techook) {
+      const techook = result.techook;
+      const randomIndex = Math.floor(Math.random() * techook.length);
+      newsItem = techook[randomIndex];
+    } else {
+      newsItem = result;
+    }
     
-   if (newsItem) {
-  await message.send(newsItem.image, {
-    value: [{ name: "quick_reply", display_text: "next", id: `technews ${match}`, style: "reply" }],
-    body: `*TITLE:* ${newsItem.title}\n*LINK:* ${newsItem.link}`,
-    footer: "*JARVIS-MD*",
-    title: "*TOP TECH NEWS*",
-    type: "image"
-  }, "button");
+    if (newsItem) {
+      await message.send(`*TITLE:* ${newsItem.title}\n*LINK:* ${newsItem.link}`, {
+        image: newsItem.image,
+        footer: "*JARVIS-MD*",
+        title: "*TOP TECH NEWS*"
+      });
     } else {
       await message.reply(`*No news found for* ${topic}`);
     }
